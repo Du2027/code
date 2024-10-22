78b1e8daaf0635fcdb0273ed0588a670286baa7e
pmac <paul-luca.k@proton.me>
2024-10-22 15:10:13 +0100

    files

---
+++ CLI/colors.c
@@ -0,0 +1,11 @@
+int main(int argc, char *argv[]){
+
+  char beginn_red[] = "\x1b[1;31m";
+  char beginn_green[] = "\x1b[1;32m";
+  char beginn_yellow[] = "\x1b[1;33m";
+  char beginn_blue[] = "\x1b[1;34m";
+  char beginn_purple[] = "\x1b[1;35m";
+  char beginn_cyan[] = "\x1b[1;36m";
+  char end_color[] = "\x1b[0m";
+
+}
