899982ef1574fc474a954747787cab0101ef0008
p <paul-luca.k@proton.me>
2024-10-27 20:00:37 +0100

    created binaries in cli/putils/bin/linux and began psort

---
+++ CLI/putils/ascii.h
@@ -0,0 +1,6 @@
+#ifndef ASCII_H
+#define ASCII_H
+
+char ascii_at(int value);
+
+#endif
---
+++ CLI/putils/psort.c
@@ -0,0 +1,4 @@
+int* psort_int(int arr[], int size){
+   int sorted_arr[size];   // wont work, would either be static, wich returns error or just return changed parameter?
+   return sorted_arr;
+}
