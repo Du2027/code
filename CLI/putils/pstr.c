1edb788e3d1ffb69d270025b65d28156a2c5e904
p <paul-luca.k@proton.me>
2024-11-11 19:11:08 +0100

    added pstr lib

---
+++ CLI/putils/pstr.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "pstr.h"
+
+int** mk_2d_int(int sizex, int sizey){
+   int** arr_2d = malloc(sizey * sizeof(int*));
+   for(int i = 0; i < sizey; i++){
+      arr_2d[i] = malloc(sizex * sizeof(int));
+   }
+   return arr_2d;
+}
+
+void rm_2d_int(int** arr_2d, int sizey){
+   for (int i = 0; i < sizey; i++) {
+      free(arr_2d[i]);
+   }
+   free(arr_2d);
+}
